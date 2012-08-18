3b438b8f5bab3152e9b93372cfb41cafb118b834
Eric Wimberley <blank89@gmail.com>
2012-08-18 19:17:48 -0400

    task cleanup for heap canaries

--- secure_instrumentation.h
+++ secure_instrumentation.h
@@ -4,11 +4,11 @@
 //-----------------------------
 //-----------------------------
 //turn on "-finstrument-functions" to use these
-//XXX turn off if this isn't the gnu compiler?
-//TODO can these be inlined?
 #include <stdlib.h>
 #include <time.h>
 #include <limits.h>
+
+#define ___MAX_CANARIES 10000
 
 class ___Canary{
 unsigned int canary;
@@ -27,10 +27,10 @@ ___Canary :: ___Canary()
 {
 	srand(time(NULL));
 	canary = rand() % UINT_MAX + 1;
-	//debug value
-	//canary = 0xffffffff;
 	test = NULL;
-	//test = new ___Canary(canary);
+	//XXX is it possible to overwrite this with a repeating value?
+	//XXX if so, the canary can be defeated (depends on object creation order?)
+	test = new ___Canary(canary);
 }
 
 ___Canary :: ___Canary(unsigned int check) : test(NULL), canary(check) 
@@ -47,17 +47,24 @@ void ___Canary :: check()
 	}	
 }
 
-//XXX set this to max stack size?
-___Canary* canaries[100];
+___Canary* canaries[___MAX_CANARIES];
 int numCanaries = 0;
+//XXX test overflow system
+int canaryOverflow = 0;
 
+//TODO can these be inlined?
 void __cyg_profile_func_enter(void *this_fn, void *call_site)
                               __attribute__((no_instrument_function));
 
 void __cyg_profile_func_enter(void *this_fn, void *call_site) {
 	//printf("ENTER: %p, from %p\n", this_fn, call_site);
-	canaries[numCanaries] = new ___Canary();  
-  	numCanaries++;
+	if(numCanaries != ___MAX_CANARIES && canaryOverflow == 0){
+		canaries[numCanaries] = new ___Canary();  
+	  	numCanaries++;
+	}
+	else{
+		canaryOverflow++;
+	}
 }
 
 void __cyg_profile_func_exit(void *this_fn, void *call_site)
@@ -65,9 +72,14 @@ void __cyg_profile_func_exit(void *this_fn, void *call_site)
 
 void __cyg_profile_func_exit(void *this_fn, void *call_site) {
 	//printf("EXIT:  %p, from %p\n", this_fn, call_site);
-	canaries[numCanaries-1]->check();  
-	free(canaries[numCanaries]);
-	numCanaries--;
+	if(numCanaries != ___MAX_CANARIES && canaryOverflow == 0){
+		canaries[numCanaries-1]->check();  
+		free(canaries[numCanaries]);
+		numCanaries--;
+	}
+	else{
+		canaryOverflow--;
+	}
 }
 
 
