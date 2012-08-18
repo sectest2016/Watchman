15ff0e974e8f1cc2770578410cce7bb9736d7828
Eric Wimberley <blank89@gmail.com>
2012-08-18 16:21:10 -0400

    first version of heap cookies using instrumentation works perfectly :)

--- secure_instrumentation.h
+++ secure_instrumentation.h
@@ -17,42 +17,57 @@ ___Canary* test;
 public:
 	
 	___Canary() __attribute__ ((no_instrument_function));
-	
-	//void ___Canary() __attribute__ ((no_instrument_function, alias ("___Canary")));
 
-	//___Canary(unsigned int test) : test(NULL), canary(test) __attribute__ ((no_instrument_function));
+	___Canary(unsigned int check) __attribute__ ((no_instrument_function));
 
-	/*void check(){
-		if(test != NULL){
-			
-		}
-	}*/
-
+	void check() __attribute__ ((no_instrument_function));
 };
 
 ___Canary :: ___Canary() 
 {
 	srand(time(NULL));
-	//canary = rand() % UINT_MAX + 1;
-	canary = 0xffffffff;
+	canary = rand() % UINT_MAX + 1;
+	//debug value
+	//canary = 0xffffffff;
 	test = NULL;
 	//test = new ___Canary(canary);
 }
+
+___Canary :: ___Canary(unsigned int check) : test(NULL), canary(check) 
+{
+
+}
+
+void ___Canary :: check()
+{
+	if(test != NULL){
+		if(test->canary != canary){
+			STACK_CHECK_FAIL;
+		}
+	}	
+}
+
+//XXX set this to max stack size?
+___Canary* canaries[100];
+int numCanaries = 0;
 
 void __cyg_profile_func_enter(void *this_fn, void *call_site)
                               __attribute__((no_instrument_function));
 
 void __cyg_profile_func_enter(void *this_fn, void *call_site) {
-	printf("ENTER: %p, from %p\n", this_fn, call_site);
-	___Canary* canary = new ___Canary();  
-  
+	//printf("ENTER: %p, from %p\n", this_fn, call_site);
+	canaries[numCanaries] = new ___Canary();  
+  	numCanaries++;
 }
 
 void __cyg_profile_func_exit(void *this_fn, void *call_site)
                              __attribute__((no_instrument_function));
 
 void __cyg_profile_func_exit(void *this_fn, void *call_site) {
-	printf("EXIT:  %p, from %p\n", this_fn, call_site);
+	//printf("EXIT:  %p, from %p\n", this_fn, call_site);
+	canaries[numCanaries-1]->check();  
+	free(canaries[numCanaries]);
+	numCanaries--;
 }
 
 
