ecbbebabb87a8f4f0fe536462a60b2d3bf1a47c2
Eric Wimberley <blank89@gmail.com>
2012-08-18 02:59:43 -0400

    more bash tools, GNU v2 applied to free parts

--- watchman.h
+++ watchman.h
@@ -1,11 +1,26 @@
-#ifndef SECURE_T_MINUS_ONE
-#define SECURE_T_MINUS_ONE
+/*
+Watchman - A memory corruption detection and security system for GCC.
+Copyright (C) 2012 Eric Wimberley
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+#ifndef WATCHMAN
+#define WATCHMAN
 
 #include <stdio.h>
-//#include <stdlib.h>
-//#include <unistd.h>
-//#include <ctype.h>
-//#include <string.h>
 #include <stdarg.h>
 #include <stdint.h>
 
@@ -16,13 +31,19 @@
 //-----------------------------
 //#define AGGRESSIVE_STACK_CHECKING
 //#define SUPER_AGGRESSIVE_STACK_CHECKING
+//#define AGGRESSIVE_HEAP_CHECKING
 //#define INSTRUMENTATION
 
 //more parts of the library
 #include "secure_manual_stack_check.h"
 #include "secure_common.h"
+
+//XXX is this needed?
 //#include "secure_instrumentation.h"
+
+#ifdef AGGRESSIVE_HEAP_CHECKING 
 #include "secure_object_protection.h"
+#endif /*AGGRESSIVE_HEAP_CHECKING*/
 
 #ifdef AGGRESSIVE_STACK_CHECKING 
 #include "secure_aggressive_stack_check.h"
@@ -33,4 +54,4 @@
 #include "paid_secure_super_aggressive_stack_checking.h"
 #endif /*SUPER_AGGRESSIVE_STACK_CHECKING*/
 
-#endif /*SECURE_T_MINUS_ONE*/
+#endif /*WATCHMAN*/
