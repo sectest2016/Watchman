2ef11d1ec3520c15dcb376de7cef2fae4adfbede
Eric Wimberley <blank89@gmail.com>
2012-08-18 14:56:35 -0400

    cleanup and almost done with first version of heap canaries

--- secure_instrumentation.h
+++ secure_instrumentation.h
@@ -3,22 +3,51 @@
 //function instrumentation?
 //-----------------------------
 //-----------------------------
-#ifdef INSTRUMENTATION
 //turn on "-finstrument-functions" to use these
 //XXX turn off if this isn't the gnu compiler?
 //TODO can these be inlined?
-//TODO make an option to turn these off
+#include <stdlib.h>
+#include <time.h>
+#include <limits.h>
+
+class ___Canary{
+unsigned int canary;
+___Canary* test;
+
+public:
+
+	___Canary(){
+		srand(time(NULL));
+		canary = rand() % UINT_MAX + 1;
+		test = new ___Canary(canary);
+	}
+
+	___Canary(unsigned int test) : test(NULL), canary(test){
+
+	}
+
+	void check(){
+		if(test != NULL){
+		
+		}
+	}
+
+};
 
 void __cyg_profile_func_enter(void *this_fn, void *call_site)
                               __attribute__((no_instrument_function));
+
 void __cyg_profile_func_enter(void *this_fn, void *call_site) {
-  printf("ENTER: %p, from %p\n", this_fn, call_site);
-} /* __cyg_profile_func_enter */
+	printf("ENTER: %p, from %p\n", this_fn, call_site);
+	___Canary* canary = new ___Canary();  
+  
+}
 
 void __cyg_profile_func_exit(void *this_fn, void *call_site)
                              __attribute__((no_instrument_function));
+
 void __cyg_profile_func_exit(void *this_fn, void *call_site) {
-  printf("EXIT:  %p, from %p\n", this_fn, call_site);
-} /* __cyg_profile_func_enter */
-#endif /*INSTRUMENTATION*/
+	printf("EXIT:  %p, from %p\n", this_fn, call_site);
+}
+
 
