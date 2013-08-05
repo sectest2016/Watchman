f86926e5eeb1513a2e6d081daac4bee0fa8bb452
Ubuntu <ewimberley@ubuntu.(none)>
2013-08-04 17:28:30 -0700

    Take two...

--- ld_test.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-(LD_PRELOAD="/lib/i386-linux-gnu/libdl.so.2 ./nightwatchman.so" gdb ./unprotected_heap.bin)
-#(LD_PRELOAD="/lib/i386-linux-gnu/libdl.so.2 ./nightwatchman.so" ./unprotected_heap_proxy.bin)
--- ld_test_debug.sh
+++
@@ -1,2 +0,0 @@
-export LD_PRELOAD="/lib/i386-linux-gnu/libdl.so.2 ./nightwatchman.so"
-gdb ./unprotected_heap.bin
